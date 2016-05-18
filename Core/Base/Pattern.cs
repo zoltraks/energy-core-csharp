2f5417af4009d6a1604739fd57b03592eca32c48
Filip Golewski <filip.golewski@animex.pl>
2016-05-18 09:52:45 +0200

    Url pattern

--- Core/Base/Pattern.cs
+++ Core/Base/Pattern.cs
@@ -7,12 +7,12 @@ namespace Energy.Base
     /// <summary>
     /// Regular expression pattern constants
     /// </summary>
-    public class Pattern
+    public static class Pattern
     {        
         /// <summary>
         /// Connection string pattern
         /// </summary>
-        public static string ConnectionString = "(?<key>{[^}]*}|[^;=\\r\\n]+)(?:=(?<value>\"(?:\"\"|[^\"])*\"|{[^}]*}|[^;]*))?(?:;)?";
+        public static readonly string ConnectionString = "(?<key>{[^}]*}|[^;=\\r\\n]+)(?:=(?<value>\"(?:\"\"|[^\"])*\"|{[^}]*}|[^;]*))?(?:;)?";
 
         /// <summary>
         /// Geolocation string containg longitude and latitude values.
@@ -24,7 +24,7 @@ namespace Energy.Base
         /// 51,5074 N 0,1278 W
         /// -51,5074 -0,1278
         /// </summary>
-        public static string LatitudeAndLongitude = @"(?:(?<latitude_name>[A-Za-z][^:]*):\s*)?
+        public static readonly string LatitudeAndLongitude = @"(?:(?<latitude_name>[A-Za-z][^:]*):\s*)?
 (?:
 (?:
 (?<latitude_degree>[-+]?\d+(?:[.,]\d+)?)\s*°\s*
@@ -54,11 +54,21 @@ namespace Energy.Base
         /// <summary>
         /// Date pattern (year + month + day)
         /// </summary>
-        public static string Date = @"(?<year>\d{4})\-(?<month>\d{1,2})\-(?<day>\d{1,2})|(?<day>\d{1,2})\-(?<month>\d{1,2})\-(?<year>\d{4})";
+        public static readonly string Date = @"(?<year>\d{4})\-(?<month>\d{1,2})\-(?<day>\d{1,2})|(?<day>\d{1,2})\-(?<month>\d{1,2})\-(?<year>\d{4})";
 
         /// <summary>
         /// Time pattern (hour + minute + second + fraction)
         /// </summary>
-        public static string Time = @"(((?<hour>\d{1,2}):)?(?<minute>\d{1,2}):)?(?<second>\d{1,2})[,\.](?<fraction>\d{1,6})|((?<hour>\d{1,2}):(?<minute>\d{1,2}))(:(?<second>\d{1,2}))?";
+        public static readonly string Time = @"(((?<hour>\d{1,2}):)?(?<minute>\d{1,2}):)?(?<second>\d{1,2})[,\.](?<fraction>\d{1,6})|((?<hour>\d{1,2}):(?<minute>\d{1,2}))(:(?<second>\d{1,2}))?";
+
+        /// <summary>
+        /// URL pattern (protocol + host + port + address)
+        /// </summary>
+        public static readonly string Url = @"(?:(?<protocol>\w+)://)?(?<host>[\w\d_\-\.]+)(?:\:(?<port>\d+))?(?<path>/[^?\r\n]*)?(?:\?(?<query>[^#\r\n]*))?";
+
+        /// <summary>
+        /// URI pattern (scheme + user + password + host + port + path + query + fragment)
+        /// </summary>
+        public static readonly string Uri = @"(?:(?<scheme>\w+):)?(?://)?(?:(?<user>[^:@\r\n]*)(?::(?<password>[^:@\r\n]*))?@)?(?<host>[\w\d_\-\.]+)(?:\:(?<port>\d+))?(?<path>/[^?\r\n]*)?(?:?(?<query>[^#\r\n]*))?(?:\#(?<fragment>[^\r\n]*))?";
     }
 }
---
+++ Core/Base/Url.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace Energy.Base
+{
+    public class Url
+    {
+        /// <summary>
+        /// Protocol
+        /// </summary>
+        public string Scheme;
+        /// <summary>
+        /// Server name
+        /// </summary>
+        public string Host;
+        /// <summary>
+        /// Port number
+        /// </summary>
+        [DefaultValue(0)]
+        public int Port;
+        /// <summary>
+        /// Path
+        /// </summary>
+        public string Path;
+        /// <summary>
+        /// Query
+        /// </summary>
+        public string Query;
+
+        /// <summary>
+        /// Represent URL structure as string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string result = Host;
+            if (Port > 0)
+            {
+                if (String.IsNullOrEmpty(result))
+                    result = "localhost";
+                result += ":" + Port.ToString();
+            }
+            if (!String.IsNullOrEmpty(Path))
+            {
+                if (!String.IsNullOrEmpty(result) && !Path.StartsWith("/"))
+                    result += "/";
+                result += Path;
+            }
+            if (!String.IsNullOrEmpty(Query))
+            {
+                result += "?" + Query;
+            }
+            if (!String.IsNullOrEmpty(Scheme))
+                result = Scheme + "://" + result;
+            return result;
+        }
+
+        /// <summary>
+        /// Create Url structure object from text
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static Url Explode(string url)
+        {
+            string pattern = Energy.Base.Pattern.Url;
+            Regex r = new Regex(pattern);
+            Match m = r.Match(url);
+            if (!m.Success)
+                return null;
+            Energy.Base.Url result = new Energy.Base.Url();
+            result.Scheme = m.Groups["protocol"].Value;
+            result.Host = m.Groups["host"].Value;
+            result.Port = Energy.Base.Cast.AsInteger(m.Groups["port"].Value);
+            result.Path = m.Groups["path"].Value;
+            result.Query = m.Groups["query"].Value;
+            return result;
+        }
+
+        /// <summary>
+        /// Create object from string representation
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static implicit operator Url(string url)
+        {
+            return Explode(url);
+        }
+
+        /// <summary>
+        /// Set host in URL text
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="host"></param>
+        /// <returns></returns>
+        public static string SetHost(string url, string host)
+        {
+            Energy.Base.Url x = url;
+            x.Host = host;
+            return x.ToString();
+        }
+
+        /// <summary>
+        /// Set port in URL text
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public static string SetPort(string url, int port)
+        {
+            Energy.Base.Url x = url;
+            x.Port = port;
+            return x.ToString();
+        }
+
+        /// <summary>
+        /// Set host and port in URL text
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public static string SetHostAndPort(string url, string host, int port)
+        {
+            Energy.Base.Url x = url;
+            x.Host = host;
+            x.Port = port;
+            return x.ToString();
+        }
+    }
+}
