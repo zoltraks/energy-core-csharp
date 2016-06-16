32980531da5fa6936a4829949d96db4c6a2e57f1
Filip Golewski <zoltarx@o2.pl>
2016-06-16 10:38:05 +0200

    Test project directory

---
+++ Test/ByteMangleTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ByteMangleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Energy.Core {0}", Energy.Core.Version.LibraryVersion);
+            
+            byte e = 0x89;
+            Console.WriteLine("Value:      {0}", Energy.Base.Hex.ByteToHex(e));
+            Console.WriteLine("High:       {0}", Energy.Base.Nibble.High(e));
+            Console.WriteLine("Low:        {0}", Energy.Base.Nibble.Low(e));
+            Console.WriteLine("Reverse:    {0}", Energy.Base.Hex.ByteToHex(Energy.Base.Nibble.Reverse(e)));
+
+            byte eb = Energy.Base.Bcd.ToByte(e);
+            Console.WriteLine("BCD as HEX: {0}", Energy.Base.Hex.ByteToHex(eb));
+            Console.WriteLine("Decimal:    {0}", eb);
+
+            byte g = 0x58;
+            Console.WriteLine("Next:       {0}", Energy.Base.Hex.ByteToHex(g));
+            Console.WriteLine("Decimal:    {0}", g);
+            Console.WriteLine("BCD:        {0}", Energy.Base.Hex.ByteToHex(Energy.Base.Bcd.FromByte(g)));
+
+            ushort w = 0x1234;
+            Console.WriteLine("Word:       {0}", w);
+            Console.WriteLine("BCD:        {0}", Energy.Base.Bcd.ToWord(w));
+
+            Console.ReadLine();
+        }
+    }
+}
