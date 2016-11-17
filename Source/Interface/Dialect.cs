dd528df0e12f37ed4f3aad45ccaf3ebbe391fc2a
Filip Golewski <zoltarx@o2.pl>
2016-11-17 18:49:44 +0100

    Energy.Source

---
+++ Source/Interface/Dialect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Energy.Source.Interface
+{
+    public interface Dialect
+    {
+        string Create();
+        string Insert();
+        string Update();
+        string Upsert();
+    }
+}
