7ae2ca680d6a661005cce5b881902edbfc1dd932
Filip Golewski <zoltarx@o2.pl>
2016-12-09 18:15:15 +0200

    Removed Energy.Bit

--- Core/Core/Configuration.cs
+++ Core/Core/Configuration.cs
@@ -9,6 +9,41 @@ namespace Energy.Core
     /// </summary>
     public class Configuration : Energy.Interface.ILoadFromFile
     {
+        public class Option
+        {
+            public string Name;
+            public bool Switch;
+            public string Value;
+
+            public class List : List<Option>
+            {
+                /// <summary>
+                /// Add an argument to a list or return existing one if found.
+                /// </summary>
+                /// <param name="name">Argument name</param>
+                /// <returns>Argument object</returns>
+                public Option Add(string name)
+                {
+                    Option item = Find(x => x.Name == name);
+                    if (item == null)
+                    {                        
+                        item = new Core.Configuration.Option() { Name = name };
+                        base.Add(item);
+                    }
+                    return item;
+                }
+            }
+        }
+
+        public void Parse(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Option.List _Options = new Option.List();
+        /// <summary>Arguments</summary>
+        public Option.List Options { get { return _Options; } set { _Options = value; } }
+
         public class Element
         {
             public string Name;
