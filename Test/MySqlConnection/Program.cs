f67047ff998583d76249c38812938b529d987968
Filip Golewski <zoltarx@o2.pl>
2017-05-07 11:50:03 +0200

    Energy.Core.Test.sln

---
+++ Test/MySqlConnection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySqlConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Energy.Source.Connection<MySql.Data.MySqlClient.MySqlConnection> connection = new Energy.Source.Connection<MySql.Data.MySqlClient.MySqlConnection>(Energy.Enumeration.SqlDialect.MySQL);
+            connection.ConnectionString = @"Server=127.0.0.1;Database=test;Uid=test;Pwd=test;";
+            connection.Open();
+            Console.WriteLine(connection.Scalar("SELECT CURRENT_TIMESTAMP()"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test/PostgresConnection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgresConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Energy.Source.Connection<Npgsql.NpgsqlConnection> c = new Energy.Source.Connection<Npgsql.NpgsqlConnection>();
+
+            //c.Configuration.Dialect ="ansi"
+            c.ConnectionString = @"Server=127.0.0.1;Port=5432;Database=trace;User Id=test;Password=test;";
+            Console.WriteLine(Energy.Base.Cast.BoolToString(c.Open(), Energy.Enumeration.BooleanStyle.YesNo));
+            var x = c.Fetch(@"SELECT * FROM ""Trace"".""Registration""");
+            Console.WriteLine(x.ToString());
+
+        }
+    }
+}
