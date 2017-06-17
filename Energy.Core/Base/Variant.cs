dc8400c56ce42155b90ffc62a81a655301d02c8d
Filip Golewski <zoltarx@o2.pl>
2017-06-17 07:13:18 +0300

    .

---
+++ Energy.Core/Base/Variant.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Energy.Base
+{
+    public class Variant
+    {
+        [StructLayout(LayoutKind.Explicit)]
+        public struct Union
+        {
+            [FieldOffset(0)]
+            public byte UInt8;
+
+            [FieldOffset(0)]
+            public SByte SInt8;
+
+            [FieldOffset(0)]
+            public UInt16 UInt16;
+
+            [FieldOffset(0)]
+            public Int16 SInt16;
+
+            [FieldOffset(0)]
+            public UInt32 UInt32;
+
+            [FieldOffset(0)]
+            public Int32 SInt32;
+
+            [FieldOffset(0)]
+            public UInt64 UInt64;
+
+            [FieldOffset(0)]
+            public Int64 SInt64;
+
+            [FieldOffset(0)]
+            public float Float;
+
+            [FieldOffset(0)]
+            public double Double;
+
+            [FieldOffset(0)]
+            public decimal Decimal;
+
+            [FieldOffset(0)]
+            public DateTime DateTime;
+
+            [FieldOffset(0)]
+            public char Char;
+
+            [FieldOffset(0)]
+            public string String;
+
+            [FieldOffset(0)]
+            public object Object;
+
+            [FieldOffset(0)]
+            public object[] Array;
+        }
+
+        public enum Type
+        {
+            UInt8,
+            SInt8,
+            UInt16,
+            SInt16,
+            UInt32,
+            SInt32,
+            UInt64,
+            SInt64,
+            Float,
+            Double,
+            Decimal,
+            DateTime,
+            Char,
+            String,
+            Object,
+            Array,
+        }
+    }
+}
--- Energy.Core/Properties/AssemblyInfo.cs
+++ Energy.Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Energy Core")]
+[assembly: AssemblyTitle("Energy Core Library")]
 [assembly: AssemblyDescription("How much reality can you take?")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.03.14.*")]
-[assembly: AssemblyFileVersion("17.03.0")]
+[assembly: AssemblyVersion("2017.06.17.*")]
+[assembly: AssemblyFileVersion("17.06.0")]
