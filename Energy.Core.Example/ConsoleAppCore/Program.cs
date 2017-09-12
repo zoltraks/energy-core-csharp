9532a291c38aa0a84c6bc52a4970964a3f471b48
Filip Golewski <zoltarx@o2.pl>
2017-09-12 15:00:29 +0200

    2017-09-12 (Berlin)

---
+++ Energy.Core.Example/ConsoleAppCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Energy.Core.Tilde.WriteLine(Energy.Core.Tilde.Color.Magenta + "Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
