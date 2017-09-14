74bbc3c2dbdf78cf372e82248cb9079c1423de0f
Filip Golewski <zoltarx@o2.pl>
2017-09-15 01:32:54 +0200

    2017-09-15 (Amsterdam)

---
+++ Energy.Core.Example/NancyApplicationApi/Module/ApiModule.cs
@@ -0,0 +1,19 @@
+﻿namespace NancyApplicationApi.Module
+{
+    using Nancy;
+
+    public class ApiModule : NancyModule
+    {
+        public ApiModule(): base("api")
+        {
+            Get["/"] = parameters =>
+            {
+                return Response.AsJson<object>(new
+                {
+                    list = "List1",
+                    orderBy = "sort_by",
+                });
+            };
+        }
+    }
+}
---
+++ Energy.Core.Example/NancyApplicationApi/Module/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace NancyApplicationApi.Module
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
