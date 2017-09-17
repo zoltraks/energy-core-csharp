f6c5d9228b13756cfb1efa7b3e9fa055a03ef3c7
Filip Golewski <zoltarx@o2.pl>
2017-09-18 00:47:19 +0200

    IEncapsulation added

---
+++ Energy.Core/Interface/IEncapsulation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Energy.Interface
+{
+    /// <summary>
+    /// Encapsulation for byte arrays interface.
+    /// </summary>
+    public interface IEncapsulation
+    {
+        /// <summary>
+        /// Encapsulate data by packing it.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        byte[] Pack(byte[] data);
+
+        /// <summary>
+        /// Decapsulate data by unpacking it.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        byte[] Unpack(byte[] data);
+    }
+}
