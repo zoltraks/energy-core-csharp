62ebcba0a684d97f6fc660f0f345af08278cdf8b
Filip Golewski <zoltarx@o2.pl>
2017-09-27 22:54:24 +0200

    2017-09-27 4

--- Build.cmd
+++ Build.cmd
@@ -1,7 +1,10 @@
 @ECHO OFF 
 REM SET PATH=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727;%PATH%
-SET PATH=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
-SET PATH=%SystemRoot%\Microsoft.NET\Framework\v3.5;%PATH%
+REM SET PATH=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
+REM SET PATH=%SystemRoot%\Microsoft.NET\Framework\v3.5;%PATH%
+
+REM @ECHO ON
+REM MSBuild Energy.Core.sln /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v2.0
 
 @ECHO ON
-MSBuild Energy.Core.sln /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v2.0
+dotnet build -c Release Energy.Core.sln
---
+++ Energy.Core.Test/Base/Text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Energy.Core.Test.Base
+{
+    [TestClass]
+    public class Text
+    {
+        [TestMethod]
+        public void FormatWithNull()
+        {
+            string value = string.Format("{0} {1}", null, null).Trim();
+            Assert.AreEqual(0, value.Length);
+        }
+
+        [TestMethod]
+        public void Join()
+        {
+            string glue = "-";
+            string format = "{0}{1}";
+            string[] a1 = new string[] { "A" };
+            string[] a2 = new string[] { "A", "A" };
+            string t = Energy.Base.Text.Join(glue, format, a1, a2);
+            Assert.AreEqual("AA-A", t);
+        }
+    }
+}
