5244ccd080ad39bfaee27f44cf88467061bf70d7
Filip Golewski <zoltarx@o2.pl>
2017-09-27 22:45:00 +0200

    2017-09-27 3

---
+++ Energy.Core.Example/WritePlatformVersion/NET20/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WritePlatformVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Version: {0}", Environment.Version.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Energy.Core.Example/WritePlatformVersion/NETCORE20/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WritePlatformVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Energy.Core.Tilde.WriteLine("~yellow~Welcome to ~white~Energy.Core ~yellow~library and ~white~.NET platform ~yellow~version ~cyan~check~yellow~...");
+            Console.WriteLine();
+            Console.WriteLine("Platform: \t{0}", Environment.Version.ToString());
+            Console.WriteLine("Library:  \t{0}", Energy.Core.Version.LibraryVersion);
+            Console.WriteLine();
+        }
+    }
+}
