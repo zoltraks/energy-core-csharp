8ed8ed133006cc76a2205bed7250c1c76e621908
Filip Golewski <zoltarx@o2.pl>
2017-10-07 17:13:30 +0200

    2017-10-07 1

--- Energy.Core/Core/Web.cs
+++ Energy.Core/Core/Web.cs
@@ -15,16 +15,39 @@ namespace Energy.Core
         /// <summary>
         /// Perform GET and return string from URL.
         /// </summary>
+        /// <param name="method"></param>
         /// <param name="url"></param>
+        /// <param name="body"></param>
         /// <param name="contentType"></param>
+        /// <param name="acceptType"></param>
+        /// <param name="encoding"></param>
+        /// <param name="detectEncodingFromByteOrderMarks"></param>
         /// <returns></returns>
-        public static string Get(string url, string contentType)
+        public static string Rest(string method, string url, string body, string contentType, string acceptType
+            , System.Text.Encoding encoding, bool detectEncodingFromByteOrderMarks)
         {
+            if (encoding == null)
+                encoding = System.Text.Encoding.UTF8;
+
             HttpWebRequest request;
             request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-            request.ContentType = contentType;
-            request.ContentLength = 0;
+            request.Method = method;
+            if (!string.IsNullOrEmpty(contentType))
+                request.ContentType = contentType;
+            if (!string.IsNullOrEmpty(acceptType))
+                request.Accept = acceptType;
+
+            byte[] data = null;
+
+            if (string.IsNullOrEmpty(body))
+            {
+                request.ContentLength = 0;
+            }
+            else
+            {
+                data = encoding.GetBytes(body);
+                request.ContentLength = data.Length;
+            }
 
             WebResponse webResponse;
             webResponse = request.GetResponse();
@@ -34,12 +57,36 @@ namespace Energy.Core
                 if (webStream == null)
                     return null;
 
-                using (StreamReader responseReader = new StreamReader(webStream))
+                if (data != null)
+                    webStream.Write(data, 0, data.Length);
+
+                using (StreamReader responseReader = new StreamReader(webStream, encoding, detectEncodingFromByteOrderMarks))
                 {
                     string response = responseReader.ReadToEnd();
                     return response;
                 }
             }
+        }
+
+        /// <summary>
+        /// Perform GET and return string from URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="acceptType"></param>
+        /// <returns></returns>
+        public static string Get(string url, string acceptType)
+        {
+            return Rest("GET", url, null, null, acceptType, null, true);
+        }
+
+        /// <summary>
+        /// Perform GET and return string from URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string Get(string url)
+        {
+            return Rest("GET", url, null, null, null, null, true);
         }
     }
 }
