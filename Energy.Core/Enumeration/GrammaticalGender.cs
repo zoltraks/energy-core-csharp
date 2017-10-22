35e9d2057de5eca5ab6ffac786e98de2eadbebe9
Filip Golewski <zoltarx@o2.pl>
2017-10-22 09:28:40 +0200

    2017-10-22 1 Enumeration GrammaticalGender added (Szczecin)

---
+++ Energy.Core/Enumeration/GrammaticalGender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Energy.Enumeration
+{
+    /// <summary>
+    /// Basic grammatical gender type in masculine–feminine–neuter system
+    /// </summary>
+    public enum GrammaticalGender
+    {
+        /// <summary>
+        /// Masculine
+        /// </summary>
+        Masculine,
+
+        /// <summary>
+        /// Feminine
+        /// </summary>
+        Feminine,
+
+        /// <summary>
+        /// Neuter
+        /// </summary>
+        Neuter,
+
+        /// <summary>
+        /// Short form of Masculine
+        /// </summary>
+        M = Masculine,
+
+        /// <summary>
+        /// Short form of Feminine
+        /// </summary>
+        F = Feminine,
+
+        /// <summary>
+        /// Short form of Neuter
+        /// </summary>
+        N = Neuter,
+    }
+}
