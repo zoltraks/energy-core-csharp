15cca7e175b84566a97b53c9de2a6a92ca66a274
Filip Golewski <zoltarx@o2.pl>
2017-11-22 11:55:01 +0100

    2017-11-22 Additional convertions example project

---
+++ Energy.Core.Example/TypeConvertion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeConvertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Energy.Core.Bug.Write("Bug message");
+            Console.ReadLine();
+        }
+    }
+}
