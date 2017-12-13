3c60628d3433dc548d173aa5faab334f20b48b7e
Filip Golewski <zoltarx@o2.pl>
2017-12-13 19:41:47 +0100

    2017-12-13 1

---
+++ Energy.Core.Example/TuppleSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TuppleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            (string country, string capital, double gdpPerCapita) = ("Malawi", "Lilongwe", 226.50);
+
+            Console.WriteLine($"Country: {country} Capital: {capital} GDP: {gdpPerCapita}");
+
+            (string a, _, string b) = GetAbc();
+
+            Console.WriteLine($"A: {a} B: {b}");
+
+            Console.ReadLine();
+        }
+
+        public static Tuple<string, string, string> GetAbc()
+        {
+            return new Tuple<string, string, string>("a", "b", "c");
+        }
+    }
+}
