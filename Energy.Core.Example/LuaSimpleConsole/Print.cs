bd6caefa82cdee564f1bb6c085e93d78094b81dd
Filip Golewski <zoltarx@o2.pl>
2017-12-16 14:36:00 +0100

    2017-12-16 3 LuaSimpleConsole example

---
+++ Energy.Core.Example/LuaSimpleConsole/Print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LuaSimpleConsole
+{
+    public static class Print
+    {
+        public static void Welcome()
+        {
+            Energy.Core.Tilde.WriteLine("~w~Welcome to ~c~Lua Simple Console ~w~ program. ~s~Enter \"~g~h~s~\" to print help message.");
+        }
+
+        public static void Help()
+        {
+
+        }
+    }
+}
---
+++ Energy.Core.Example/LuaSimpleConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LuaSimpleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print.Welcome();
+
+            using (Neo.IronLua.Lua lua = new Neo.IronLua.Lua())
+            {
+                var state = lua.CreateEnvironment();
+
+                string name = "test.lua";
+                state.DoChunk("message = 'Hello World!'", name); // create a variable in lua
+                state.DoChunk("message = 'Hello World 2!'", name); // create a variable in lua
+                Console.WriteLine(state["message"]); // access a variable in c#
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
