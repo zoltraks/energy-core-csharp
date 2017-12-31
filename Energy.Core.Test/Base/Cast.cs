b8dfc47a920220a5ebde0490b7d6fba4a4dd8ecf
Filip Golewski <zoltarx@o2.pl>
2017-12-31 09:49:04 +0100

    2017-12-31 1 Package update

--- Energy.Core.Test/Base/Cast.cs
+++ Energy.Core.Test/Base/Cast.cs
@@ -9,13 +9,15 @@ namespace Energy.Core.Test.Base
         [TestMethod]
         public void StringToDouble()
         {
-            // arrange 
             string str1 = "15,001";
             double num1 = Energy.Base.Cast.StringToDouble(str1);
+            Assert.AreEqual(15.001, num1);
             string str2 = " 15.000000001 ";
             double num2 = Energy.Base.Cast.StringToDouble(str2);
-            Assert.AreEqual(15.001, num1);
             Assert.AreEqual(15.000000001, num2);
+            string str3 = " - 1 ,234 ";
+            double num3 = Energy.Base.Cast.StringToDouble(str2);
+            Assert.AreEqual(-1.234, num3);
         }
     }
 }
