b95516fca6e693b036c527df8dc80331d2ef5a89
Filip Golewski <zoltarx@o2.pl>
2018-01-01 20:40:57 +0100

    2018-01-01 1 Base64 example

---
+++ Energy.Core.Example/Base64UTF8Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Base64UTF8Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] array = new string[] { "Hello", "password", "", "Zażółć gęślą jaźń" };
+            foreach (string input in array)
+            {
+                string base64 = Energy.Base.Cast.StringToBase64(input);
+                string text = Energy.Base.Cast.Base64ToString(base64);
+                System.Console.WriteLine("Text: \"{0}\", Base64: \"{1}\", Text: \"{2}\"", input, base64, text);
+            }
+            Energy.Core.Tilde.Pause();
+        }
+    }
+}
