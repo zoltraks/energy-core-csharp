c6150fefa13ec82cfb26e792493e711f39c0a516
Filip Golewski <zoltarx@o2.pl>
2018-01-22 22:27:45 +0100

    HexPrint example project

---
+++ Energy.Core.Example/HexPrint/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HexPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Energy.Core.Tilde.Pause();
+        }
+
+        private static void Test()
+        {
+            byte[] array = Energy.Base.Random.GetRandomByteArray(60, 41, 99);
+            string print;
+            print = Energy.Base.Hex.Print(array);
+            Energy.Core.Tilde.WriteLine(print);
+        }
+    }
+}
