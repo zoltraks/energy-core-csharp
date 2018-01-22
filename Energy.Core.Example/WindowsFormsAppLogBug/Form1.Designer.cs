ebf57f6b47bf99073fe947f9687a7abdcb63aa69
Filip Golewski <zoltarx@o2.pl>
2018-01-22 22:27:33 +0100

    WindowsFormsAppLogBug example project

---
+++ Energy.Core.Example/WindowsFormsAppLogBug/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsAppLogBug
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAddMessage = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonAddMessage
+            // 
+            this.buttonAddMessage.Location = new System.Drawing.Point(12, 12);
+            this.buttonAddMessage.Name = "buttonAddMessage";
+            this.buttonAddMessage.Size = new System.Drawing.Size(110, 23);
+            this.buttonAddMessage.TabIndex = 0;
+            this.buttonAddMessage.Text = "Add message";
+            this.buttonAddMessage.UseVisualStyleBackColor = true;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessage.Location = new System.Drawing.Point(128, 14);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(144, 20);
+            this.textBoxMessage.TabIndex = 1;
+            this.textBoxMessage.Text = "Example message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonAddMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAddMessage;
+        private System.Windows.Forms.TextBox textBoxMessage;
+    }
+}
+
---
+++ Energy.Core.Example/WindowsFormsAppLogBug/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppLogBug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Energy.Core.Bug.Write("Form Load");
+
+            //Energy.Core.Log.Default.Destination += new Energy.Log.Target.Event();
+            Energy.Core.Log logger = new Energy.Core.Log();
+            logger.Maximum = 10;
+        }
+    }
+}
