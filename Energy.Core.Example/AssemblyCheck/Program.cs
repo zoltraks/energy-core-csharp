1a68c1db2cdebd169cf8f09a1821c826485a471e
Filip Golewski <f.golewski@gmail.com>
2018-05-10 12:16:47 +0200

    Energy.Core.Example/AssemblyCheck

---
+++ Energy.Core.Example/AssemblyCheck/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AssemblyCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test.GetAllAssemblies();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Energy.Core.Example/AssemblyCheck/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AssemblyCheck
+{
+    public class Test
+    {
+        public static void GetAllAssemblies()
+        {
+            Assembly[] assemblies = Energy.Base.Class.GetAssemblies();
+            if (assemblies == null)
+                return;
+            foreach (Assembly assembly in assemblies)
+            {
+                Console.WriteLine(assembly.CodeBase);
+                Console.WriteLine(assembly.FullName);
+                Console.WriteLine(assembly.Location);
+                Module manifestModule = assembly.ManifestModule;
+                if (manifestModule != null)
+                {
+                    Console.WriteLine(manifestModule.Name);
+                }
+                Console.WriteLine("---");
+            }
+        }
+    }
+}
