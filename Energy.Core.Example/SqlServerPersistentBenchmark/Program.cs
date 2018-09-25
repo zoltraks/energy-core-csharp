3e010b1c3fd25b63ed2b187eea9aff1fd6a2dc11
Filip Golewski <f.golewski@gmail.com>
2018-09-25 09:26:47 +0200

    Introducing new example project

---
+++ Energy.Core.Example/SqlServerPersistentBenchmark/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlServerPersistentBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (!Setup(args))
+                {
+                    return;
+                }
+                Go(args);
+            }
+            catch (Exception x)
+            {
+                if (!string.IsNullOrEmpty(x.Message))
+                {
+                    Console.WriteLine("Exception: {0}", x.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static bool Setup(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Go(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
