da354a424bd97eaefa84406327f9e1a0256935b5
Filip Golewski <f.golewski@gmail.com>
2018-10-03 22:11:05 +0200

    Sql Persistent connection check Test

--- Energy.Core.Example/SqlServerPersistentBenchmark/Test.cs
+++ Energy.Core.Example/SqlServerPersistentBenchmark/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace SqlServerPersistentBenchmark
@@ -8,7 +9,14 @@ namespace SqlServerPersistentBenchmark
     {
         internal static void GetDate()
         {
-            //string connStr1 = 
+            string connStr = "Data Source=.\\SQLEXPRESS;Initial Catalog=master;Integrated Security=Yes;Connect Timeout=10;";
+            connStr = Energy.Core.Tilde.Ask("Connection string", connStr);
+            Energy.Source.Connection<SqlConnection> source = new Energy.Source.Connection<SqlConnection>(connectionString: connStr);
+            source.Persistent = false;
+            int num1 = Energy.Core.Benchmark.Loop(() => { string result = source.Scalar<string>("SELECT GETDATE()"); }, 1);
+            source.Persistent = true;
+            int num2 = Energy.Core.Benchmark.Loop(() => { string result = source.Scalar<string>("SELECT GETDATE()"); }, 1);
+            Console.WriteLine($"Dynamic {num1} Persistent {num2}");
         }
     }
 }
