8a8374587d4f0ffefd42fc11bf3a024ad0b0a393
Filip Golewski <f.golewski@gmail.com>
2018-11-04 18:37:13 +0100

    Example project LogFileThreadStress

---
+++ Energy.Core.Example/LogFileThreadStress/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogFileThreadStress
+{
+    public class App : Energy.Interface.ICommandProgram
+    {
+        public bool Initialize(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Run(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Setup(string[] args)
+        {
+            Energy.Core.Log.Logger logger = new Energy.Core.Log.Logger();
+
+            string path = Energy.Core.Application.GetExecutionPath(System.Reflection.Assembly.GetExecutingAssembly());
+
+            logger.Destination.Add(new Energy.Core.Log.Target.File()
+            {
+                Path = path,
+                Immediate = true,
+                Background = true,
+            });
+
+            logger.Destination.Add(new Energy.Core.Log.Target.Console()
+            {
+                Immediate = true,
+                Background = true,
+            });
+
+            logger.Write("SETUP");
+
+            return true;
+        }
+    }
+}
---
+++ Energy.Core.Example/LogFileThreadStress/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogFileThreadStress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Energy.Core.Application app = new Energy.Core.Application(typeof(App));
+            app.Run();
+            Console.ReadLine();
+        }
+    }
+}
