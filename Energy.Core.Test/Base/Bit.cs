ab1dd2d060342f4b2982404c35d5ca0be7a3968f
Filip Golewski <f.golewski@gmail.com>
2018-12-03 20:08:27 +0100

    Energy.Base.Bit

---
+++ Energy.Core.Test/Base/Bit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Energy.Core.Test.Base
+{
+    [TestClass]
+    public class Bit
+    {
+        [TestMethod]
+        public void Endianess()
+        {
+            uint _uint32;
+            int _int32;
+
+            _uint32 = Energy.Base.Bit.GetUInt32MSB(49440);
+            Assert.AreEqual(49440, _uint32);
+            _int32 = (int)_uint32;
+            Assert.AreEqual(-16672, _int32);
+            _uint32 = Energy.Base.Bit.GetUInt32MSB();
+            Assert.AreEqual(0, _uint32);
+            _uint32 = Energy.Base.Bit.GetUInt32MSB(5085, 49440);
+            Assert.AreEqual(333300000, _uint32);
+        }
+    }
+}
