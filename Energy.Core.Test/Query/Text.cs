63a0b5ae32944f003e8c80b9a5c277dd31543440
Filip Golewski <f.golewski@gmail.com>
2019-03-07 02:59:09 +0100

    Energy.Query.Text.Strip

--- Energy.Core.Test/Query/Text.cs
+++ Energy.Core.Test/Query/Text.cs
@@ -43,5 +43,33 @@ namespace Energy.Core.Test.Query
             result = Energy.Query.Text.Quote(value, "[]");
             Assert.AreEqual(expect, result);
         }
+
+        [TestMethod]
+        public void TextStrip()
+        {
+            string expect;
+            string value;
+            string result;
+
+            value = null;
+            expect = null;
+            result = Energy.Query.Text.Strip(value);
+            Assert.AreEqual(expect, result);
+
+            value = "null";
+            expect = null;
+            result = Energy.Query.Text.Strip(value);
+            Assert.AreEqual(expect, result);
+
+            value = "'a''b'";
+            expect = "a'b";
+            result = Energy.Query.Text.Strip(value);
+            Assert.AreEqual(expect, result);
+
+            value = "[a[0]]]";
+            expect = "a[0]";
+            result = Energy.Query.Text.Strip(value);
+            Assert.AreEqual(expect, result);
+        }
     }
 }
--- Energy.Core/Query/Text.cs
+++ Energy.Core/Query/Text.cs
@@ -10,6 +10,8 @@ namespace Energy.Query
     /// </summary>
     public class Text
     {
+        #region Quote
+
         /// <summary>
         /// Quote string value using apostrophe (') as quotation mark.
         /// Function will return "NULL" if value is null.
@@ -117,5 +119,85 @@ namespace Energy.Query
             string result = string.Concat(prefix, value, suffix);
             return result;
         }
+
+        #endregion
+
+        #region Strip
+
+        /// <summary>
+        /// Strip quotation from a value. 
+        /// Includes support for apostrophes, quotation marks, square brackets or unquoted values.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string Strip(string value)
+        {
+            if (value == null)
+                return null;
+            value = value.Trim();
+            if (value.Length == 0)
+                return value;
+            if (value.StartsWith("["))
+                return Strip(value, "[]", "NULL");
+            if (value.StartsWith("'"))
+                return Strip(value, "'", "NULL");
+            else
+                return Strip(value, @"""", "NULL");
+        }
+
+        /// <summary>
+        /// Strip quotation from a value.
+        /// Return null if nullText parameter is specified and value equals to it.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="quote"></param>
+        /// <param name="nullText"></param>
+        /// <returns></returns>
+        public static string Strip(string value, string quote, string nullText)
+        {
+            if (value == null)
+                return null;
+            value = value.Trim();
+            if (value.Length == 0)
+                return value;
+            if (!string.IsNullOrEmpty(nullText) && 0 == string.Compare(value, nullText, true))
+                return null;
+            if (string.IsNullOrEmpty(quote))
+                return value;
+            if (quote.Length == 1)
+            {
+                if (value.Length < 2)
+                    return value;
+                if (value.StartsWith(quote) && value.EndsWith(quote))
+                {
+                    value = value.Substring(1, value.Length - 2);
+                }
+                if (value.Contains(quote))
+                {
+                    value = value.Replace(quote + quote, quote);
+                }
+                return value;
+            }
+            if (0 != quote.Length % 2)
+            {
+                throw new NotSupportedException("Quotation mark string must be of even length");
+            }
+            int half = quote.Length >> 1;
+            string prefix = quote.Substring(0, half);
+            string suffix = quote.Substring(half);
+            if (value.StartsWith(prefix) && value.EndsWith(suffix))
+            {
+                value = value.Substring(prefix.Length, value.Length - prefix.Length - suffix.Length);
+                if (value.Contains(suffix))
+                {
+                    string replacement = suffix + suffix;
+                    value = value.Replace(replacement, suffix);
+                }
+                return value;
+            }
+            return value;
+        }
+
+        #endregion
     }
 }
