771b015039a651793dca06057b87d2a85f17a98e
Filip Golewski <f.golewski@gmail.com>
2019-11-11 21:27:29 +0100

    ...

---
+++ Energy.Core/Base/Unit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Energy.Base
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Unit
+    {
+        public class Unit
+        {
+
+        }
+
+        /// <summary>
+        /// Represents system of units.
+        /// </summary>
+        public class System
+        {
+
+        }
+
+        /// <summary>
+        /// Represents symbol for a unit.
+        /// </summary>
+        public class Symbol
+        {
+
+        }
+
+        /// <summary>
+        /// Represents equivalent of a unit
+        /// </summary>
+        public class Equivalent
+        {
+
+        }
+
+        /// <summary>
+        /// Represents conversion between units.
+        /// </summary>
+        public class Conversion
+        {
+
+        }
+    }
+}
---
+++ Energy.Core/Enumeration/UnitQuanity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Energy.Enumeration
+{
+    /// <summary>
+    /// Unit
+    /// </summary>
+    public enum UnitQuanity
+    {
+        Amount,
+        Time,
+        /// <summary>
+        /// Energy, work and heat
+        /// </summary>
+        Energy,
+        Mass,
+        Length,
+        Area,
+        Volume,
+        Temperature,
+        Cost,
+        Voltage,
+        Power,
+        Frequency,
+        Capacity,
+        Force,
+        Angle,
+        Pressure,
+        Luminance,
+        Level,
+    }
+}
