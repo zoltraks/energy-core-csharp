fd06cf5582b24081aacebe8d26d5d00a509281f1
Filip Golewski <f.golewski@gmail.com>
2020-12-30 21:04:20 +0100

    Energy.Core/Interface/IUnescape.cs

---
+++ Energy.Core/Interface/IUnescape.cs
@@ -0,0 +1,10 @@
+﻿namespace Energy.Interface
+{
+    /// <summary>
+    /// Interface for escaping texts with quotes or surrounded in any way.
+    /// </summary>
+    public interface IUnescape
+    {
+        string Unescape(string text);
+    }
+}
