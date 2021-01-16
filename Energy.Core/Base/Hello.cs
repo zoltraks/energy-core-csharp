71aa8fba571754e21e16487acea0db41f66c74b2
Filip Golewski <f.golewski@gmail.com>
2021-01-16 14:28:56 +0100

    Energy.Base.Hello.World()

---
+++ Energy.Core/Base/Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Energy.Base
+{
+    public static class Hello
+    {
+        public static string World()
+        {
+            return "Hello, World.";
+        }
+    }
+}
