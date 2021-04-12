6c90cf8f0131894f7ed0a6f86af9fc49b9d569eb
Energy <energy.core@outlook.com>
2021-04-12 22:12:09 +0200

    Energy.Core.Compact 20.12.16

--- Energy.Core/Properties/AssemblyInfo.cs
+++ Energy.Core/Properties/AssemblyInfo.cs
@@ -44,7 +44,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.12.15")]
+[assembly: AssemblyVersion("20.12.16")]
 #if !NETCF
 [assembly: AssemblyFileVersion("20.12.11")]
 #endif
