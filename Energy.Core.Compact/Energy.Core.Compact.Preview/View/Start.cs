f3527a24968b87d4269ef0e4b266ef7716c887e6
Energy <energy.core@outlook.com>
2025-12-27 00:44:45 +0100

    Energy.Core.Compact directory for Compact Framework support

---
+++ Energy.Core.Compact/Energy.Core.Compact.Preview/View/Start.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Energy.Core.Compact.Example.View
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+    }
+}
